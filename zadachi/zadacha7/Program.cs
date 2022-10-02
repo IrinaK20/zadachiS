83eb7c4b9522a5442a689db42cdbd209d27c7f17
irina <irinakyleashova2519@gmail.com>
2022-10-03 01:27:16 +0300

    z7

---
+++ zadachi/zadacha7/Program.cs
@@ -0,0 +1,10 @@
+﻿/*принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа*/
+
+Console.WriteLine("введите трехзначное число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+while(n<100|n>999)
+{
+    Console.Write("недопустимое число, повторите ввод: ");
+    n=Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("поседняя цифра числа:" + Convert.ToString(n%10));
