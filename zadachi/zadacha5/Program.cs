3fdf7436344b1fc5a8cb4ff0894cd16c5a9b5623
irina <irinakyleashova2519@gmail.com>
2022-10-03 01:02:29 +0300

    z5

---
+++ zadachi/zadacha5/Program.cs
@@ -0,0 +1,11 @@
+﻿/* программа, на входринимает число (А), а на выходе показывает все 
+цеые числа в промежутке от -А до А */
+
+Console.WriteLine ("введите число:");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int i=n*(-1);
+while(i<=n)
+{
+    Console.Write (Convert.ToString(i) + " " );
+    i++;
+}
