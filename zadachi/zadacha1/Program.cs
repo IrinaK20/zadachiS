147441c594af97925b928b5f4eec58c4fb4f9b5c
irina <irinakyleashova2519@gmail.com>
2022-10-02 20:06:53 +0300

    z1

---
+++ zadachi/zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*программа, которая на вход принимает два числа и проверяет,
+ является ли первое число квадратом второго*/
+
+Console.WriteLine ("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine ("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+if(a==b*b)
+{
+  Console.WriteLine ("yes");  
+}
+else
+{
+    Console.WriteLine ("no");
+}
